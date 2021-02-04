eaaa0c42320203565a6fca6bb2290791b9421043
Sebastian Krysmanski <noreply@manski.net>
2021-02-04 12:24:22 +0100

    Added reproducer

--- ReSharperReproducer/Reproducer.cs
+++ ReSharperReproducer/Reproducer.cs
@@ -2,13 +2,28 @@
 // This file contains a reproducer for a ReSharper bug.
 //
 
+using System;
+
+using JetBrains.Annotations;
+
 namespace ReSharperReproducer
 {
     public class Reproducer
     {
-        public void ReproducerMethod()
-        {
+        private readonly byte[] _publicKeyBytes;
 
+        private readonly byte[] _privateKeyBytes;
+
+        public Reproducer(byte[] publicKeyBytes, byte[] privateKeyBytes)
+        {
+            this._publicKeyBytes = publicKeyBytes;
+            this._privateKeyBytes = privateKeyBytes;
+        }
+
+        [MustUseReturnValue]
+        public (ReadOnlyMemory<byte> publicKeyBytes, ReadOnlyMemory<byte> privateKeyBytes) ReproducerMethod()
+        {
+            return (this._publicKeyBytes, this._privateKeyBytes);
         }
     }
 }
